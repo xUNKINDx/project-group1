490bb838585cdbcb3d7b55ac8706f34e89899dde
polrmn <polrmn@gmail.com>
2022-11-24 19:27:05 +0200

    add swiper script

---
+++ src/js/swiper.js
@@ -0,0 +1,12 @@
+new Swiper('.swiper', {
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
+    },
+
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+});
+
