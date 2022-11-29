cc77d1e51040f95275337cbfcdce066b29ea25cc
xUNKINDx <112956710+xUNKINDx@users.noreply.github.com>
2022-11-29 14:40:05 +0000

    Deploying to gh-pages from @ xUNKINDx/project-group1@0e32a5255a7c7a0de3adacae2803ead86a0481a4 🚀

---
+++ index.d1ce21bf.js
@@ -0,0 +1,2 @@
+new Swiper(".swiper",{pagination:{el:".swiper-pagination",clickable:!0},autoplay:{delay:3e3,stopOnLastSlide:!1,disableOnInteraction:!0},keyboard:{enabled:!0,onlyInViewport:!0},loop:!0});
+//# sourceMappingURL=index.d1ce21bf.js.map
