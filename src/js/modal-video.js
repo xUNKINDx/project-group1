6d7ceff8a854fb621ce57220cad24dea76c3699d
Chumak Slavik <chumakslav@gmail.com>
2022-11-30 17:21:42 +0200

    rework

--- src/js/modal-video.js
+++ src/js/modal-video.js
@@ -1,20 +1,19 @@
 (() => {
-    const refs = {
-      openModalBtn: document.querySelector("[data-modal-open]"),
-      closeModalBtn: document.querySelector("[data-modal-close]"),
-      modal: document.querySelector("[data-modal]"),
-    };
-  
-    refs.openModalBtn.addEventListener("click", toggleModal);
-    refs.closeModalBtn.addEventListener("click", toggleModal);
-  
-    function toggleModal() {
-      refs.modal.classList.toggle("is-hidden");
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-open-video]'),
+    closeModalBtn: document.querySelector('[data-modal-close-video]'),
+    modal: document.querySelector('[data-modal-video]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+  }
+  window.addEventListener('click', function (e) {
+    if (!refs.modal.classList.contains('is-hidden') && e.target == refs.modal) {
+      refs.modal.classList.add('is-hidden');
     }
-    window.addEventListener("click", function (e) {
-      if (!refs.modal.classList.contains('is-hidden') && e.target == refs.modal) {
-        refs.modal.classList.add("is-hidden");
-      }
-    });
-  })();
-  
+  });
+})();
