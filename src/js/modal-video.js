8bc89f357b13bdfb7ab35719eaa52a369545b303
Chumak Slavik <chumakslav@gmail.com>
2022-11-30 17:59:39 +0200

    rework

--- src/js/modal-video.js
+++ src/js/modal-video.js
@@ -3,6 +3,7 @@
     openModalBtn: document.querySelector('[data-modal-open-video]'),
     closeModalBtn: document.querySelector('[data-modal-close-video]'),
     modal: document.querySelector('[data-modal-video]'),
+    bodyTag: document.querySelector('body'),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
@@ -10,10 +11,12 @@
 
   function toggleModal() {
     refs.modal.classList.toggle('is-hidden');
+    refs.bodyTag.classList.toggle('scroll-off');
   }
   window.addEventListener('click', function (e) {
     if (!refs.modal.classList.contains('is-hidden') && e.target == refs.modal) {
       refs.modal.classList.add('is-hidden');
+      refs.bodyTag.classList.toggle('scroll-off');
     }
   });
 })();
--- src/js/modal.js
+++ src/js/modal.js
@@ -1,14 +1,16 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector("[data-modal-open]"),
-    closeModalBtn: document.querySelector("[data-modal-close]"),
-    modal: document.querySelector("[data-modal]"),
+    openModalBtn: document.querySelector('[data-modal-open]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
+    bodyTag: document.querySelector('body'),
   };
 
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
-    refs.modal.classList.toggle("is-hidden");
+    refs.modal.classList.toggle('is-hidden');
+    refs.bodyTag.classList.toggle('scroll-off');
   }
 })();
