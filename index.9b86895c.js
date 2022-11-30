599deb7afd994197760dac82863ba232b606de29
xUNKINDx <112956710+xUNKINDx@users.noreply.github.com>
2022-11-30 16:01:17 +0000

    Deploying to gh-pages from @ xUNKINDx/project-group1@8bc89f357b13bdfb7ab35719eaa52a369545b303 🚀

---
+++ index.9b86895c.js
@@ -0,0 +1,2 @@
+!function(){var o={openModalBtn:document.querySelector("[data-modal-open]"),closeModalBtn:document.querySelector("[data-modal-close]"),modal:document.querySelector("[data-modal]"),bodyTag:document.querySelector("body")};function e(){o.modal.classList.toggle("is-hidden"),o.bodyTag.classList.toggle("scroll-off")}o.openModalBtn.addEventListener("click",e),o.closeModalBtn.addEventListener("click",e)}();
+//# sourceMappingURL=index.9b86895c.js.map
